13e1c40e06700aa0d4b06bd6e84a4a298236ea08
claudiocbamt <claudiocbamt@gmail.com>
2021-09-12 12:01:54 -0300

    update footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/claudiocbamt" target="_blank" rel="noreferrer">Nelio Alves</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                    Evento promovido pela escola DevSuperior: <a href="claudiocbamt@gmail.com" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
         </footer>
     );
